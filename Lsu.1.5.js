864553c34ac2f66e8b005ef82d1f39da09ee621a
macrylynn <530454062@qq.com>
2016-04-18 21:39:50 +0800

    4-18日版本

---
+++ Lsu.1.5.js
@@ -0,0 +1,18 @@
+function aaaa() {
+    document.write("<h1>这是标题</h1>");
+    document.write("<p>这是段落。</p>");
+    document.write("<p>这是另一个段落。</p>");
+    var txt = "Hello World!"
+    document.write(txt.length)
+}
+
+function bu(a) {
+    a = a * 3;
+    alert(a);
+
+
+}
+function buu(){
+    
+
+}
