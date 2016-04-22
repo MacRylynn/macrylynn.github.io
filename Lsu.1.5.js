daf140b342d67136a13bfcd8267bab7a3605e58d
macrylynn <530454062@qq.com>
2016-04-22 18:31:23 +0800

    画了一个iphone删掉了一些东西
    Signed-off-by: macrylynn <530454062@qq.com>

--- Lsu.1.5.js
+++ Lsu.1.5.js
@@ -1,18 +1,4 @@
-function aaaa() {
-    document.write("<h1>这是标题</h1>");
-    document.write("<p>这是段落。</p>");
-    document.write("<p>这是另一个段落。</p>");
-    var txt = "Hello World!"
-    document.write(txt.length)
-}
+function function1() {
 
-function bu(a) {
-    a = a * 3;
-    alert(a);
-
-
-}
-function buu(){
-    
-
+    alert("炸弹即将爆炸!!")
 }
